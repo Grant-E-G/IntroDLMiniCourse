d6c38628887f5bcb1518dca1dcd0f4b7aff2821b
G_G <GEGcodeprojects@gmail.com>
2018-11-10 16:37:55 -0600

    Refactored and cleaned up stats output

--- lesson1_qlearning/qlearninglake.py
+++ lesson1_qlearning/qlearninglake.py
@@ -13,7 +13,7 @@ def main():
     state_size = env.observation_space.n
     qtable = np.zeros((state_size, action_size))
 
-    total_episodes = 30000        # Total episodes
+    total_episodes = 15000        # Total episodes
     learning_rate = 0.8           # Learning rate
     max_steps = 99                # Max steps per episode
     gamma = 0.95                  # Discounting rate
@@ -22,49 +22,52 @@ def main():
     epsilon = 1.0                 # Exploration rate
     max_epsilon = 1.0             # Exploration probability at start
     min_epsilon = 0.01            # Minimum exploration probability
-    decay_rate = 0.003            # Exponential decay rate for exploration prob
+    decay_rate = 0.005            # Exponential decay rate for exploration prob
 
     """
-	Code block copied from Thomas Simonini tutoral on RL
-	Unique code below
-	"""
-    reward_trials = 2000  # when report value the number of trials to average over
-    
+    Code block copied from Thomas Simonini tutoral on RL
+    Unique code below
+    """
+    post_training_trials = 2000  # Number of post traing experimental trials
+
     total_reward = 0.0
     for x in range(total_episodes):
-        #Reset and grab the first observation
+        # Reset and grab the first observation
         state = env.reset()
-        #Set epsilon for our epsilon greedy search
+        # Set epsilon for our epsilon greedy search
         if epsilon < min_epsilon:
             epsilon = min_epsilon
         else:
             epsilon = max_epsilon * np.exp(-x * decay_rate)
         for stepcount in range(max_steps):
-            #The exploration case
+            # The exploration case
             if epsilon >= random.uniform(0, 1):
-                action = random.randrange(0, action_size)
-                new_state, reward, done, info = env.step(action)
-                total_reward += reward
-                qtable[state][action] = updateqtable(
-                    state, new_state, action, reward, qtable, learning_rate, gamma)
-                state = new_state
-            #The exploit case
+                action = action = env.action_space.sample()
+            # The exploit case
             else:
-                action = np.argmax(qtable[state])
-                new_state, reward, done, info = env.step(action)
-                total_reward += reward
-                qtable[state][action] = updateqtable(
-                    state, new_state, action, reward, qtable, learning_rate, gamma)
-                state = new_state
+                # Random tie breaking
+                action = np.random.choice(np.flatnonzero(
+                    qtable[state] == qtable[state].max()))
+            # perform the action
+            new_state, reward, done, info = env.step(action)
+            total_reward += reward
+            qtable[state][action] = updateqtable(
+                state, new_state, action, reward, qtable, learning_rate, gamma)
+            state = new_state
             if done:
                 break
-
+    # Output data + stats
     print(qtable)
-    print('Mean post learning reward is')
-    print(meanreward(env, qtable, max_steps, reward_trials))
-    print('Mean reward over trials')
-    print(total_reward/total_episodes)
-    
+    print('Average reward during learning')
+    print(total_reward / total_episodes)
+    print('Average reward post learning over ' +
+          str(post_training_trials) + ' trials')
+    data = qtablestats(env, qtable, max_steps, post_training_trials)
+    print(np.mean(data))
+    print('Varance of post learning reward over ' +
+          str(post_training_trials) + ' trials')
+    print(np.var(data))
+
     pass
 
 
@@ -73,21 +76,23 @@ def updateqtable(state, new_state, action, reward, qtable, learning_rate, gamma)
     deltaq = learning_rate * (reward + gamma * maxq - qtable[state][action])
     return qtable[state][action] + deltaq
 
-#Run the algorithm after training to examine our average reward.
-def meanreward(env, qtable, max_steps, reward_trials):
-    mean_reward = 0.0
-    for x in range(reward_trials):
-        reward_total = 0.0
+# Run the algorithm after training to examine our results.
+
+
+def qtablestats(env, qtable, max_steps, post_training_trials):
+    data = np.array([0])
+    for x in range(post_training_trials):
         state = env.reset()
+        total_reward = 0.0
         for stepcount in range(max_steps):
             action = np.argmax(qtable[state])
             state, reward, done, info = env.step(action)
-            reward_total += reward
+            total_reward += reward
             if done:
+                data = np.append(data, total_reward)
                 break
-        mean_reward += reward_total / reward_trials
-
-    return mean_reward
+    # Hack here to drop the first dummy value
+    return data[1:]
 
 
 if __name__ == '__main__':
